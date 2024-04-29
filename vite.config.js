5e72e83e81f655e24ab8112a712dffe1d22a6b79
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-04-29 12:11:42 +0100

    added type module in pakage json file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
+       
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
