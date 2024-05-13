5c80c343bdb629ae3b9952e95aec6068cc2f2d96
mrchigozieike <70382310+mrchigozieike@users.noreply.github.com>
2024-05-13 21:36:07 +0100

    correct

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,10 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  console.log(cartItems);
+  const cartItems = getLocalStorage('so-cart');
+  
   const htmlItems = Array.of(cartItems).map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
 function cartItemTemplate(item) {
